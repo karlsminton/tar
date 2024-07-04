40b17fe14f4094d53ede23d49c8affa8fec103d2
Karl Minton <kminton@frontier.co.uk>
2024-07-04 20:07:41 +0100

    minor tweaks

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 app = express();
+const port = '3003';
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
 });
 
-app.listen('3003', () => {
-    console.log('hi');
+app.listen(port, () => {
+    console.log('webapp listening on port %s', 3003);
 });
